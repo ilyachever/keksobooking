b032984bc184fa4068555a0a991a64a8b8d2c11c
Artur I <ilyachever@gmail.com>
2023-01-19 18:43:48 +0400

    Added mock data

---
+++ js/mock.js
@@ -0,0 +1,117 @@
+import { getPositiveRandomInteger, getPositiveRandomFloat, getPositiveRandomUniqueInteger, getRandomArrayElement } from "./util.js";
+
+const userIdValues = {
+  MIN: 1,
+  MAX: 10,
+}
+
+const userId = getPositiveRandomUniqueInteger(userIdValues.MIN, userIdValues.MAX);
+
+const offerTitles = [
+  'Уютное гнездышко для молодоженов',
+  'Небольшая лавочка в парке',
+  'Чёткая хата',
+  'Милое гнездышко для фанатов Анимэ',
+  'Маленькая квартирка рядом с парком',
+  'Императорский дворец в центре Токио',
+  'Милейший чердачок',
+  'Хостел «Для друзей»',
+  'Стандартная квартира в центре',
+  'Тихая квартирка недалеко от метро',
+]
+
+const descriptionTitles = [
+  "Великолепный таун-хауз в центре Токио",
+  "Убойное место",
+  "Лучше, чем ничего",
+  "Не дороже, чем в Питере",
+  "Спасибо, что не в России",
+]
+
+const prices = {
+  min: 10000,
+  max: 100000,
+};
+
+const types = ['palace', 'flat', 'house', 'bungalowhotel'];
+
+const roomsQuantity = {
+  min: 1,
+  max: 7,
+}
+
+const guestsQuantity = {
+  min: 1,
+  max: 14,
+}
+
+const checkinTime = ['12:00', '13:00', '14:00'];
+const checkoutTime = ['12:00', '13:00', '14:00'];
+
+const featuresList = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+
+const photosList = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+]
+
+const location = {
+  MIN_LATITUDE: 35.65,
+  MAX_LATITUDE: 35.70,
+  MIN_LONGITUDE: 139.70,
+  MAX_LONGITUDE: 139.80,
+}
+
+function createAuthorData(index) {
+  return {
+    avatar: `img/avatars/user${index.toString().padStart(2, '0')}.png`,
+  }
+}
+
+function createLatitude() {
+  return getPositiveRandomFloat(location.MIN_LATITUDE, location.MAX_LATITUDE, 5)
+}
+
+function createLongitude() {
+  return getPositiveRandomFloat(location.MIN_LONGITUDE, location.MAX_LONGITUDE, 5)
+}
+
+function createLocation() {
+  return {
+    lat: createLatitude(),
+    lng: createLongitude(),
+  }
+}
+
+function createOffer() {
+  return {
+    title: getRandomArrayElement(offerTitles),
+    address: `${createLatitude()}, ${createLongitude()}`,
+    price: getPositiveRandomInteger(prices.min, prices.max),
+    type: getRandomArrayElement(types),
+    rooms: getPositiveRandomInteger(roomsQuantity.min, roomsQuantity.max),
+    guests: getPositiveRandomInteger(guestsQuantity.min, guestsQuantity.max),
+    checkin: getRandomArrayElement(checkinTime),
+    checkout: getRandomArrayElement(checkoutTime),
+    features: featuresList.slice(0, getPositiveRandomInteger(0, featuresList.length)),
+    description: getRandomArrayElement(descriptionTitles),
+    photos: photosList.slice(0, getPositiveRandomInteger(0, photosList.length)),
+  }
+}
+
+function createDataObject() {
+  return {
+    author: createAuthorData(userId()),
+    offer: createOffer(),
+    location: createLocation(),
+  }
+}
+
+const data = [];
+
+for (let i = 1; i <= 10; i++) {
+  data.push(createDataObject())
+}
+
+export {data};
---
+++ js/util.js
@@ -0,0 +1,43 @@
+function getPositiveRandomInteger(min, max) {
+  const minValue = Math.min(Math.abs(min), Math.abs(max));
+  const maxValue = Math.max(Math.abs(min), Math.abs(max));
+  const resultValue = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+
+  return resultValue;
+}
+
+function getPositiveRandomFloat(min, max, digits = 1) {
+  const minValue = Math.min(Math.abs(min), Math.abs(max));
+  const maxValue = Math.max(Math.abs(min), Math.abs(max));
+  const resultValue = (Math.random() * (maxValue - minValue + 1)) + minValue;
+
+  return +resultValue.toFixed(digits);
+}
+
+function getPositiveRandomUniqueInteger(min, max) {
+  const usedNumbers = [];
+
+  return function() {
+    let uniqueInteger = getPositiveRandomInteger(min, max);
+
+    if (usedNumbers.length >= (max - min + 1)) {
+      console.error(`Перебраны все числа из диапазона от ${min} до ${max}`);
+
+      return null;
+    }
+
+    while (usedNumbers.includes(uniqueInteger)) {
+      uniqueInteger = getPositiveRandomInteger(min, max);
+    }
+
+    usedNumbers.push(uniqueInteger);
+
+    return uniqueInteger;
+  }
+}
+
+function getRandomArrayElement(array) {
+  return array[getPositiveRandomInteger(0, array.length - 1)];
+}
+
+export {getPositiveRandomInteger, getPositiveRandomFloat, getPositiveRandomUniqueInteger, getRandomArrayElement};
