25803cd9573a33a47fff9c20b48d1a09525a921c
Artur I <ilyachever@gmail.com>
2023-02-03 22:58:14 +0400

    Added async/await requests

--- js/server.js
+++ js/server.js
@@ -1,40 +1,40 @@
+import { filterEnable } from "./state.js";
+import { showAlertMessage, hideAlertMessage } from "./util.js";
+
 const GET_DATA_LINK = 'https://23.javascript.pages.academy/keksobooking/data';
 const POST_DATA_LINK = 'https://23.javascript.pages.academy/keksobooking';
 
-const DISPLAYED_ADS_QUANTITY = 10;
+async function getData(onSuccess, adsQuantity = 5, alertDelay = 3000) {
+  try {
+    const dataResponse = await fetch(GET_DATA_LINK);
+    const data = await dataResponse.json();
 
-function getData(onSuccess) {
-  fetch(GET_DATA_LINK)
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
-
-      throw new Error(`${response.status} ${response.statusText}`)
-    })
-    .then((response) => {
-      response.slice(0, DISPLAYED_ADS_QUANTITY).forEach((item) => onSuccess(item));
-    })
-    .catch((error) => {
-      console.error(`Ошибка загрузки данных ${error.message}`);
-    })
+    data.slice(0, adsQuantity).forEach((dataItem) => onSuccess(dataItem));
+    filterEnable();
+  } catch (error) {
+    showAlertMessage(`Возникла ошибка`);
+    setTimeout(hideAlertMessage, alertDelay);
+  }
 }
 
-function sendData(onSuccess, onFail, body) {
-  fetch(POST_DATA_LINK, {
-    method: 'POST',
-    body,
-  })
-    .then((response) => {
-      if (response.ok) {
-        onSuccess();
+async function sendData(onSuccess, onError, body, alertDelay = 3000) {
+  try {
+    const data = await fetch(POST_DATA_LINK,
+      {
+        method: 'POST',
+        body,
       }
+    )
 
-      onFail();
-    })
-    .catch((error) => {
-      console.error(`Ошибка отправки данных ${error.message}`);
-    })
+    if (data.ok) {
+      onSuccess();
+    } else {
+      onError();
+    }
+  } catch (error) {
+    showAlertMessage(`Возникла ошибка`);
+    setTimeout(hideAlertMessage, alertDelay);
+  }
 }
 
 export { getData, sendData }
